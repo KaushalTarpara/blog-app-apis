0d2cd27e457408bda076688e7e9513f986352749
KdTarpara <kaushaltarpara10702@gmail.com>
2023-02-18 21:49:20 +0530

    Creat Spring Project and Connected to Database

---
+++ src/main/java/com/codewithkd/blog/BolgAppApisApplication.java
@@ -0,0 +1,14 @@
+package com.codewithkd.blog;
+
+import org.springframework.boot.SpringApplication; 
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BolgAppApisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BolgAppApisApplication.class, args);
+		System.out.println("Hello spring");
+	}
+
+}
---
+++ src/test/java/com/codewithkd/blog/BolgAppApisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codewithkd.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BolgAppApisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
